2b1b3f4509d4292ef8f1c552e77b584226e942cd
Kevin Zhang <14856464+zhangke8@users.noreply.github.com>
2017-08-09 10:50:54 -0400

    Create Labyrinth.js

---
+++ Labyrinth.js
@@ -0,0 +1,8 @@
+// source: http://js.do/
+for (var line=1; line<60; line++) {
+  for(var i=1;i<29;i++) {
+    var s = (Math.floor((Math.random()*2)%2)) ? "╱" : "╲";
+    document.write(s);
+  }
+  document.writeln("<br>");
+}
