47dd9643073140ce26b50f084a4974b54480df3e
Kevin Zhang <14856464+zhangke8@users.noreply.github.com>
2017-08-15 12:37:25 -0400

    Update automatic file downloads.py

--- Python/automatic file downloads.py
+++ Python/automatic file downloads.py
@@ -25,68 +25,20 @@ for i in fileList:
         files.append(k.strip())
 fileList.close()
 
-# are you using /w/, /wiki/ or /index.php ?
-print ("""MENU
------------------
-Option 1: /w/
-Option 2: /wiki/
-Option 3: /Index.php/
-""")
-Option = input("Please choose an option from above menu: ")
-
-if Option.lower() == "1":
-    option = "/w"
-elif Option.lower() == "2":
-    option = "/wiki"
-elif Option.lower() == "3":
-    option = "/Index.php"
-else:
-     while (Option.lower() != "1" or Option.lower() != "2" or Option.lower() != "3"):
-            Option = input("Try Again! ")
-            if Option.lower() == "1":
-                option = "/w"
-                break
-            elif Option.lower() == "2":
-                option = "/wiki"
-                break
-            elif Option.lower() == "3":
-                option = "/Index.php"
-                break
-
-mw = input("Are you using MediaWiki 1.28+? y/n ")
-
-if mw.lower() == "y":
-    special = "/Special:Filepath/"
-
-elif mw.lower() == "n":
-    special = "/Special:FilePath/"
-else:
-    while (mw.lower() != "y" or mw.lower() != "n"):
-        mw = input("Try Again! ")
-        if mw.lower() == "y":
-            special = "/Special:Filepath/"
-            break
-
-        elif mw.lower() == "n":
-            special = "/Special:FilePath/"
-            break
-
-subDomain = input("What subdomain? ")
-domain = input("What is the overall domain? ")
-
 # full url path
-url = "http://" + subDomain + "." + domain + option + special
+url = "http://yugioh.wikia.com/wiki/Special:FilePath/"
 
 urls = []
 fileNames = []
+
 for file in files:
-    if ((file != "") and (file.find(".jpg") != -1 or file.find(".JPG") != -1 or file.find(".jpeg") != -1 or file.find(".png") != -1 or file.find(".PNG")  != -1 or file.find(".gif") != -1 or file.find(".pdf") != -1 or file.find(".ogg") != -1 ) ):
+    if ((file != "") and (file.find(".jpg") != -1 or file.find(".JPG") != -1 or file.find(".jpeg") != -1 or file.find(".png") != -1 or file.find(".PNG")  != -1 or file.find(".gif") != -1 or file.find(".pdf") != -1 or file.find(".ogg")) ):
         removeNS = file.find("File:")
         url += file[removeNS+5:].replace(" ","_")
         urls.append(url)
         fileNames.append( file[removeNS+5:].replace(" ","_") ) # get all filenames
     # reset url builder
-        url = "http://" + subDomain + "." + domain + option + special
+        url = "http://yugioh.wikia.com/wiki/Special:FilePath/"
         
 count = 0 # accurately get which file is being downloaded
 URLE = []
@@ -95,17 +47,13 @@ ConnectionE = []
 notFound = []
 UnicodeEncode = []
 print("DOWNLOADING IMAGES")
-print("formats supported: jpg, jpeg, png, pdf, gif, ogg")
-print("Videos are skipped since those are from YouTube")
-print("----------------------------------------------------------------------------------------------------------")
-
 #Download the file
 for u, f in zip(urls, fileNames): # takes in 2 lists to operate on
     try:
         #u is to generate an url request, f is the filename to save it under as
         with urllib.request.urlopen(u) as response, open(f, 'wb') as out_file:
             shutil.copyfileobj(response, out_file)
-            time.sleep(3) # wait 3 seconds
+        
     # all error handling
     except urllib.error.URLError as e:
         URLE.append(f)
@@ -122,13 +70,22 @@ for u, f in zip(urls, fileNames): # takes in 2 lists to operate on
     except UnicodeEncodeError:
         UnicodeEncode.append(f)
         pass
-    
     count+= 1
-    print("finished downloading file", f, "----", count ," ...")
 
+    if (count % 1000 == 0):
+        winsound.Beep(500,1000)
+    
+    print("finished downloading file", f, "-", count)
+
+
+    
 print("Done downloading!")
+
 time.sleep(2)
-winsound.Beep(500,1000) # indicate to user finished
+
+winsound.Beep(500,3000)
+
+
 
 print("-------------------------------------------------------------------------")
 print()
